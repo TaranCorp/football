b8f8b2534e8064c97c5ea04b355cc1993ccc421a
TaranCorp <82828335+TaranCorp@users.noreply.github.com>
2022-08-22 13:31:27 +0200

    *Fix path of CsvUtils

--- build/index.js
+++ build/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchUtils_1 = require("./football/MatchUtils");
-const CsvUtils_1 = require("./Csv/CsvUtils");
+const CsvUtils_1 = require("./csv/CsvUtils");
 const matchesPath = './src/resources/football.csv';
 const matches = (0, CsvUtils_1.readMatchesFromCsv)(matchesPath);
 const matchesList = (0, MatchUtils_1.parseMatches)(matches);
