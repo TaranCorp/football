a2c948b0d1ed778c8c89e49561b6925230d599fe
TaranCorp <82828335+TaranCorp@users.noreply.github.com>
2022-08-22 13:43:46 +0200

    *Shows use case of CsvFileReader

--- build/index.js
+++ build/index.js
@@ -4,3 +4,21 @@ const CsvFileReader_1 = require("./CsvFileReader");
 const matchesPath = './src/resources/football.csv';
 const reader = new CsvFileReader_1.CsvFileReader(matchesPath);
 reader.read();
+var MatchesResult;
+(function (MatchesResult) {
+    MatchesResult["HOST_WIN"] = "H";
+    MatchesResult["GUEST_WIN"] = "A";
+    MatchesResult["DRAW"] = "D";
+})(MatchesResult || (MatchesResult = {}));
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchesResult.HOST_WIN) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchesResult.GUEST_WIN) {
+        manUnitedWins++;
+    }
+}
+console.log(`
+    Manchester United won ${manUnitedWins} matches
+`);
--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,24 @@ const matchesPath = './src/resources/football.csv';
 const reader = new CsvFileReader(matchesPath);
 reader.read();
 
+enum MatchesResult {
+    HOST_WIN = 'H',
+    GUEST_WIN = 'A',
+    DRAW = 'D'
+}
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchesResult.HOST_WIN) {
+        manUnitedWins++;
+    } else if (match[2] === 'Man United' && match[5] === MatchesResult.GUEST_WIN) {
+        manUnitedWins++;
+    }
+}
+
+console.log(`
+    Manchester United won ${manUnitedWins} matches
+`)
+
 
